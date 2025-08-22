0aa7a89535e1bb33744b418d92b29d9459bf6593
이한승 <itstrap1016@gmail.com>
2025-08-22 20:40:40 +0900

    top-nav 스타일 수정

--- components/layout/top-nav.tsx
+++ components/layout/top-nav.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function TopNavigation() {
   return (
-    <nav className="h-15 border-b-[1px] border-slate-200 max-[1248px]:px-6 max-md:px-4">
+    <nav className="h-15 border-b-[1px] border-slate-200 sticky top-0 bg-white max-[1248px]:px-6 max-md:px-4">
       <div className="max-w-[1200px] mx-auto h-full flex items-center sticky top-0">
         <h1>
           <Link href="/">
